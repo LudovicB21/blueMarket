5f945eeda79cefe6d03b07f5909b3a92fe3d224f
Ludovic Braine <ludovic.braine@winciel.fr>
2021-04-14 15:02:13 +0200

    WIP: Profile

--- src/scenes/Profile/Profile.js
+++ src/scenes/Profile/Profile.js
@@ -7,6 +7,9 @@ function Profile() {
         getAuth()
     }, [])
 
+    const [auth, setAuth] = useState("")
+    const [details, setDetails] = useState({});
+
     const getAuth = () => {
         setAuth(JSON.parse(localStorage.getItem('user')))
     }
@@ -21,9 +24,13 @@ function Profile() {
         }
     }
 
-    const [auth, setAuth] = useState("")
+    const submitHandler = e => {
+        e.preventDefault();
 
-    console.log(auth)
+        console.log(details)
+        // Appel api pour changement des données profile
+    }
+
     return (
         <div>
             <div>
@@ -31,37 +38,45 @@ function Profile() {
             </div>
             <div className="container mx-5 my-5">
                 <h1>Mon profile </h1>
-                <div className="row">
-                    <div className="col-sm form-group">
-                        <label for="firstname">Firstname :</label>
-                        <input type="text" className="form-control" value={auth.firstname}></input>
+                <a className="text-danger"> <strong> Pour changer vos données remplissez les champs </strong>  </a>
+                <form onSubmit={submitHandler} >
+                    <div className="row">
+                        <div className="col-sm form-group">
+                            <label for="firstname">Firstname :</label>
+                            <input type="text" className="form-control" onChange={e => setDetails({ ...details, "firstname": e.target.value })} placeholder={auth.firstname}></input>
+                        </div>
+                        <div className="col-sm form-group">
+                            <label for="lastname">Lastname :</label>
+                            <input type="text" className="form-control" onChange={e => setDetails({ ...details, "lastname": e.target.value })} placeholder={auth.lastname}></input>
+                        </div>
                     </div>
-                    <div className="col-sm form-group">
-                        <label for="email">Lastname :</label>
-                        <input type="text" className="form-control" value={auth.lastname}></input>
+                    <div className="row">
+                        <div className="col-sm form-group">
+                            <label for="email">Email :</label>
+                            <input type="email" className="form-control" onChange={e => setDetails({ ...details, "email": e.target.value })} placeholder={auth.email}></input>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm form-group">
-                        <label for="email">Email :</label>
-                        <input type="email" className="form-control" value={auth.email}></input>
+                    <div className="row">
+                        <div className="col-sm form-group">
+                            <label for="fridge">Fridge ( Litre ) :</label>
+                            <input type="text" className="form-control" onChange={e => setDetails({ ...details, "fridge": e.target.value })} placeholder={auth.frigo}></input>
+                        </div>
+                        <div className="col-sm form-group">
+                            <label for="role">Role :</label>
+                            <input type="text" className="form-control" disabled="true" placeholder={role()}></input>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm form-group">
-                        <label for="fridge">Fridge ( Litre ) :</label>
-                        <input type="text" className="form-control" value={auth.frigo}></input>
+                    <div className="row">
+                        <div className="col-sm form-group">
+                            <button className="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter"> Change password</button>
+                        </div>
                     </div>
-                    <div className="col-sm form-group">
-                        <label for="fridge">Role :</label>
-                        <input type="text" className="form-control" value={role()}></input>
+                    <div className="row">
+                        <div className="col-sm form-group">
+                            <button className="btn btn-primary"> Enregistrer</button>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm form-group">
-                        <button className="btn btn-primary"> Change password</button>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
     )
