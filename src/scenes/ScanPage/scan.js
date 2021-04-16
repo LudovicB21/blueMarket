6ef2d924c4a4a80fda4c8dc88cdab41c6a256cd1
Ludovic Braine <ludovic.braine@winciel.fr>
2021-04-16 16:06:31 +0200

    WIP: scan

--- src/scenes/ScanPage/scan.js
+++ src/scenes/ScanPage/scan.js
@@ -3,6 +3,7 @@ import NavBar from '../NavBar/NavBar'
 import { Redirect } from 'react-router-dom'
 import Quagga from 'quagga'
 import './scan.css'
+import { Modal, Button } from 'react-bootstrap'
 
 
 class Scanner extends Component {
@@ -13,7 +14,9 @@ class Scanner extends Component {
             nocamera: false,
             test: [],
             auth: "",
-            error: false
+            error: false,
+            show: false,
+            result: ""
         }
         this.onDetect = this.onDetect.bind(this)
     }
@@ -54,12 +57,16 @@ class Scanner extends Component {
         }
     }
 
+    handleClose = () => this.setState({ show: false });
+    handleShow = () => this.setState({ show: true });
+
     onDetect(res) {
-        console.log(res.codeResult.code)
-        alert(res.codeResult.code)
+        this.handleShow()
+        this.setState({ result: res.codeResult.code })
+        //alert(res.codeResult.code)
         //this.setState({ test: [...res.codeResult.code] })
-        Quagga.stop()
-        Quagga.offProcessed()
+        //Quagga.offProcessed()
+        //Quagga.stop()
         //this.props.onBarcodeDetect(res.codeResult.code)
     }
 
@@ -77,6 +84,25 @@ class Scanner extends Component {
                             {this.state.test}
                         </div>
                     </Fragment>
+                    <div>
+                        <button className="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter" onClick={this.handleShow}> Change password</button>
+                        <Modal size="lg" show={this.state.show} onHide={this.handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title> Ajouter votre produit </Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                {this.state.result || null}
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="success" onClick={this.handleClose}>
+                                    Valider et continuer
+                                    </Button>
+                                <Button variant="primary" onClick={this.handleClose}>
+                                    Voir le panier
+                                    </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </div>
                 </div>
             )
         }
@@ -85,84 +111,3 @@ class Scanner extends Component {
 
 export default Scanner
 
-
-/*function Scan() {
-
-    useEffect(() => {
-        authenticated()
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                constraints: {
-                    width: '790',
-                    height: '490'
-                },
-                numberOfWorkers: navigator.hardwareConcurrency,
-                target: document.querySelector('#barcodeScan')
-            },
-            locate: true,
-            decoder: {
-                readers: ["code_128_reader", "upc_reader", "upc_e_reader"]
-            }
-        }, function (err) {
-            if (err) {
-                console.log(err)
-                return
-            }
-            console.log("initialization finished. Ready to start")
-            Quagga.start()
-        })
-        Quagga.onDetected(onDetect())
-    }, [])
-
-    const onDetect = (res)  => {
-        console.log(res.codeResult.code)
-        alert(res.codeResult.code)
-        //this.setState({ test: [...res.codeResult.code] })
-        //Quagga.stop()
-        //Quagga.offProcessed()
-        //this.props.onBarcodeDetect(res.codeResult.code)
-    }
-
-
-    const [auth, setAuth] = useState("")
-    const [error, setError] = useState(false)
-    const [data, setData] = useState("");
-    const [camera, setCamera] = useState(false)
-
-    const authenticated = () => {
-        if (localStorage.getItem('user')) {
-            setAuth(JSON.parse(localStorage.getItem('user')))
-        } else {
-            setError(true)
-        }
-    }
-
-    if (error === true) {
-        return <Redirect to="/login" />
-    } else {
-        return (
-            <div>
-                <div>
-                    <NavBar />
-                </div>
-                <BarcodeScannerComponent
-                    width={500}
-                    height={500}
-                    onUpdate={(err, result) => {
-                        console.log(err)
-                        if (result) {
-                            alert(result)
-                            //setData(result.text)
-                        }
-                        else setData('Not Found')
-                    }}
-                />
-                <p>{data}</p>
-            </div>
-        )
-    }
-}
-
-export default Scan*/
