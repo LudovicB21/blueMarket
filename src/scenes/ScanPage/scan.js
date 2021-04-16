3a8b6962f124b3ef71b03a5155ea51a7c1d3070e
Ludovic Braine <ludovic.braine@winciel.fr>
2021-04-16 10:58:07 +0200

    WIP: add scan

--- src/scenes/ScanPage/scan.js
+++ src/scenes/ScanPage/scan.js
@@ -1,16 +1,121 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component, Fragment } from 'react'
 import NavBar from '../NavBar/NavBar'
 import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 import { Redirect } from 'react-router-dom'
-function Scan() {
+import Quagga from 'quagga'
+import './scan.css'
+
+
+class Scanner extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nocamera: false,
+            test: []
+        }
+        this.onDetect = this.onDetect.bind(this)
+    }
+
+    componentDidMount() {
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                constraints: {
+                    width: '790',
+                    height: '490'
+                },
+                numberOfWorkers: navigator.hardwareConcurrency,
+                target: document.querySelector('#barcodeScan')
+            },
+            locate: true,
+            decoder: {
+                readers: ["code_128_reader", "upc_reader", "upc_e_reader"]
+            }
+        }, function (err) {
+            if (err) {
+                console.log(err)
+                return
+            }
+            console.log("initialization finished. Ready to start")
+            Quagga.start()
+        })
+        Quagga.onDetected(this.onDetect)
+    }
+
+    onDetect(res) {
+        console.log(res.codeResult.code)
+        alert(res.codeResult.code)
+        //this.setState({ test: [...res.codeResult.code] })
+        //Quagga.stop()
+        //Quagga.offProcessed()
+        //this.props.onBarcodeDetect(res.codeResult.code)
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <NavBar />
+                </div>
+                <Fragment>
+                    <div id="barcodeScan">
+                        {this.state.test}
+                    </div>
+                </Fragment>
+            </div>
+        )
+    }
+}
+
+export default Scanner
+
+
+/*function Scan() {
 
     useEffect(() => {
         authenticated()
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                constraints: {
+                    width: '790',
+                    height: '490'
+                },
+                numberOfWorkers: navigator.hardwareConcurrency,
+                target: document.querySelector('#barcodeScan')
+            },
+            locate: true,
+            decoder: {
+                readers: ["code_128_reader", "upc_reader", "upc_e_reader"]
+            }
+        }, function (err) {
+            if (err) {
+                console.log(err)
+                return
+            }
+            console.log("initialization finished. Ready to start")
+            Quagga.start()
+        })
+        Quagga.onDetected(onDetect())
     }, [])
+
+    const onDetect = (res)  => {
+        console.log(res.codeResult.code)
+        alert(res.codeResult.code)
+        //this.setState({ test: [...res.codeResult.code] })
+        //Quagga.stop()
+        //Quagga.offProcessed()
+        //this.props.onBarcodeDetect(res.codeResult.code)
+    }
+
 
     const [auth, setAuth] = useState("")
     const [error, setError] = useState(false)
     const [data, setData] = useState("");
+    const [camera, setCamera] = useState(false)
 
     const authenticated = () => {
         if (localStorage.getItem('user')) {
@@ -46,4 +151,4 @@ function Scan() {
     }
 }
 
-export default Scan
+export default Scan*/
