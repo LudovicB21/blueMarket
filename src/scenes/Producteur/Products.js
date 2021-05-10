85d6c0ef0c1401da59a3242cd6944b96f2288b39
Ludovic Braine <ludovic.braine@winciel.fr>
2021-05-10 11:55:48 +0200

    advance promotion

--- src/scenes/Producteur/Products.js
+++ src/scenes/Producteur/Products.js
@@ -7,23 +7,7 @@ import promotion from '../../stores/promotion'
 
 function Products() {
 
-    useEffect(() => {
-        authenticated()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    const [auth, setAuth] = useState("")
-    const [error, setError] = useState(false)
-    const [show, setShow] = useState(false);
-    const [showPromotion, setShowPromotion] = useState(false);
-    const [detail, setDetail] = useState([])
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const handleShowPromotion = () => setShowPromotion(true)
-    const handleClosePromotion = () => setShowPromotion(false);
-
-    const data = [
+    const dataa = [
         {
             id: 1,
             produit: "lait",
@@ -60,6 +44,29 @@ function Products() {
             promotion: 1,
         },
     ]
+
+    useEffect(() => {
+        authenticated()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        setData(dataa)
+    }, [])
+
+    const [auth, setAuth] = useState("")
+    const [error, setError] = useState(false)
+    const [show, setShow] = useState(false);
+    const [showPromotion, setShowPromotion] = useState(false);
+    const [detail, setDetail] = useState([])
+    const [selectedOption, setSelectOption] = useState(null)
+    const [selectedOptionId, setSelectedOptionId] = useState(null)
+    const [data, setData] = useState([])
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleShowPromotion = id => {
+        setSelectedOptionId(id)
+        setShowPromotion(true)
+    };
+    const handleClosePromotion = () => setShowPromotion(false);
 
     const constitution = [
         {
@@ -120,13 +127,35 @@ function Products() {
         }
     }
 
-    const details = (id) => {
+    const details = id => {
         // Faire un fetch
         // eslint-disable-next-line array-callback-return
         constitution.map(item => {
             if (id === item.id) {
                 setDetail(item)
                 handleShow()
+            }
+        })
+    }
+
+    const removePromotion = id => {
+        const items = data
+        items.map(item => {
+            if (item.id === id) {
+                item.promotion = null
+            }
+        })
+        setData(items)
+    }
+
+    const handleChange = selectedOption => {
+        setSelectOption(selectedOption);
+    };
+
+    const addPromotion = () => {
+        data.map(item => {
+            if (item.id === selectedOptionId) {
+                item.promotion = selectedOption.label
             }
         })
     }
@@ -221,9 +250,12 @@ function Products() {
                                             </Button>
                                             </Modal.Footer>
                                         </Modal>
-                                        <button className="btn btn-primary" onClick={handleShowPromotion}>
+
+                                        {produits.promotion ? <button className="btn btn-danger" onClick={e => removePromotion(produits.id)}>
+                                            X
+                                    </button> : <button className="btn btn-primary" onClick={e => handleShowPromotion(produits.id)}>
                                             Promotion
-                                    </button>
+                                    </button>}
                                         <Modal size="lg" show={showPromotion} onHide={handleClosePromotion}>
                                             <Modal.Header closeButton>
                                                 <Modal.Title> Add new promotion </Modal.Title>
@@ -233,17 +265,20 @@ function Products() {
                                                     <div className="col-sm form-group">
                                                         <label htmlFor="transport">Type of promotion :</label>
                                                         <Select
-                                                            options={(promotion || null).map(item => item)}
-                                                        //onChange={handleChange}
-                                                        //value={selectedOption}
+                                                            options={promotion}
+                                                            onChange={handleChange}
+                                                            value={selectedOption}
                                                         />
                                                     </div>
                                                 </div>
                                             </Modal.Body>
                                             <Modal.Footer>
+                                                <Button variant="secondary" onClick={addPromotion}>
+                                                    Add
+                                                </Button>
                                                 <Button variant="secondary" onClick={handleClosePromotion}>
                                                     Close
-                                            </Button>
+                                                </Button>
                                             </Modal.Footer>
                                         </Modal>
                                     </td>
